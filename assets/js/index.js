a591230698304d29c9a37aaf2cc781742321ac3c
dayang4321 <dayang4321@users.noreply.github.com>
2020-07-24 21:26:20 +0100

    tm img

--- assets/js/index.js
+++ assets/js/index.js
@@ -55,3 +55,8 @@ $(window).scroll(function() {
     }
  });
 
+ /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('home', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
